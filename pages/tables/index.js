3d62ff9c96dde9e1029e294240805a2762e23120
nada <nadasamir9334@gmail.com>
2023-06-29 20:14:15 -0400

    tables work

---
+++ pages/tables/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Teamtable from '../../Components/tablescomponants/Teamtable'
+import Projecttable from '../../Components/tablescomponants/Projecttable'
+
+
+function index() {
+    return (
+        <div>
+            <Teamtable />
+            <Projecttable />
+
+        </div>
+    )
+}
+
+export default index
